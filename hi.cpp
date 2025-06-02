6f584561770a59cf8f4ad8184ef75b4a45bfa3dc
darshan19-2005 <darshan.s1@s.amity.edu>
2025-06-02 23:34:01 +0530

    i have written c++ code

--- hi.cpp
+++ hi.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+int main() {
+    // CGI response header
+    std::cout << "Content-type: text/html\n\n";
+
+    // Begin HTML output
+    std::cout << "<!DOCTYPE html>\n";
+    std::cout << "<html>\n";
+    std::cout << "<head><title>C++ HTML Output</title></head
 #include<stido.h>
 int main()
 {
